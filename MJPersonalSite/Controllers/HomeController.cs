09ca34d1ae3d1ee5ac64ac99c5f448d9a6fdd958
Matthew Jennings <mejennings2@gmail.com>
2022-10-31 09:20:20 -0500

    Add project files.

---
+++ MJPersonalSite/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MJPersonalSite.Models;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+namespace MJPersonalSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Resume()
+        {
+            return View();
+        }
+
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public IActionResult Links()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        
+        
+        
+        //private readonly IConfiguration _config;
+        ////Add a constructor for our controller that accepts the config info as a parameter
+        //public HomeController(IConfiguration config)
+        //{
+        //    _config = config;
+        //}
+
+
+    }
+}
